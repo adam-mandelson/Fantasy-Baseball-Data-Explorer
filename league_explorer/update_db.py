f12833ec7b8ec84fad9caf4298fca261137e5b24
adam.mandelson@gmail.com <adam.mandelson@gmail.com>
2022-05-06 16:22:21 -0400

    Changed database update method to insert instead of drop table

--- league_explorer/update_db.py
+++ league_explorer/update_db.py
@@ -14,10 +14,9 @@ from io import StringIO
 import psycopg2
 
 from config import config, psycopg2_exception
-from update_league_data import yearly_stats
 
 
-def connect():
+def connect(data) -> None:
     conn = None
     try:
         params = config()
@@ -28,29 +27,13 @@ def connect():
         with conn:
             cur = conn.cursor()
 
-            cur.execute("DROP TABLE IF EXISTS yearly_stats;")
-            sql = '''CREATE TABLE yearly_stats(
-                id serial,
-                season integer not null,
-                week integer not null,
-                n_days integer not null,
-                team_id integer not null,
-                matchup varchar (50) not null,
-                matchup_id varchar (50) not null,
-                category varchar (50),
-                value numeric,
-                won_category varchar (50),
-                won_week varchar (50)
-                )'''
-            cur.execute(sql)
-
         with conn:
             print('PostgreSQL database:')
             cur.execute('SELECT version()')
 
         with conn:
             buffer = StringIO()
-            yearly_stats.to_csv(buffer, header=False, index=True)
+            data.to_csv(buffer, header=False, index=True)
             buffer.seek(0)
             try:
                 cur.copy_from(buffer, 'yearly_stats', sep=",")
@@ -67,7 +50,3 @@ def connect():
         if conn is not None:
             conn.close()
             print('Database connection closed.')
-
-
-if __name__ == '__main__':
-    connect()
